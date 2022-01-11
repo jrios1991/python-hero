e21119a0a84789c3b643d85cc3c941a3a12c1e88
Rios-Olmedo <jriosolmedo@teksystems.com>
2022-01-11 10:10:10 -0600

    initial commit, arithmetic basics

---
+++ python_numbers.py
@@ -0,0 +1,8 @@
+#python mathematical operations
+print(f"addition: {2+1}")
+print(f"subtraction: {10-3}")
+print(f"multiplication: {10*3}")
+print(f"divsion: {10/5}")
+print(f"modulo: {10%5}")
+print(f"power: {10**3}")
+print(f"order of operations: {(2+17)*(2+2)}")
