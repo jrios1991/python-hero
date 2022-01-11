463bc314f813eeeeba871fff063c8f09a8f18956
Rios-Olmedo <jriosolmedo@teksystems.com>
2022-01-11 11:07:45 -0600

    intro to python variables

--- python_numbers.py
+++ python_numbers.py
@@ -1,8 +1,22 @@
 #python mathematical operations
-print(f"addition: {2+1}")
-print(f"subtraction: {10-3}")
-print(f"multiplication: {10*3}")
-print(f"divsion: {10/5}")
-print(f"modulo: {10%5}")
-print(f"power: {10**3}")
-print(f"order of operations: {(2+17)*(2+2)}")
+# print(f"addition: {2+1}")
+# print(f"subtraction: {10-3}")
+# print(f"multiplication: {10*3}")
+# print(f"divsion: {10/5}")
+# print(f"modulo: {10%5}")
+# print(f"power: {10**3}")
+# print(f"order of operations: {(2+17)*(2+2)}")
+
+# python variables
+a = 5
+print(a)
+print(a+a)
+a = a*a
+print(a)
+print(type(a))
+a = 20.1
+print(type(a))
+my_income = 2000
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
