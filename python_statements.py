7c0dc1686900c55b09318cc27774156aca80b052
jriostek <jriosolmedo@teksystems.com>
2022-03-31 15:54:43 -0500

    if elif and else

---
+++ python_statements.py
@@ -0,0 +1,14 @@
+# if elif and else statements
+hungry = True
+if hungry:
+    print("Feed me!")
+else:
+    print("I'm not hungry right now.")
+
+location = "Bank"
+if location == "Auto Shop":
+    print("I need an oil change")
+elif location == "Store":
+    print("I need to get my groceries.")
+else:
+    print(f"I need to go to the {location}")
